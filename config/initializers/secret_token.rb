90f20543e52029413ea18bd9ed975423ae77be91
Rick Clare <rick@wildcountrytech.co.uk>
2011-06-23 14:55:38 +0100

    first cut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SenchaTouchRailsSandbox::Application.config.secret_token = '781e96ff968fdf9cbbd77dfef1a8375dafe0e342370b7e576a6c7962eb607b54cb5238728b44a6ed1e8b14663d7370e47940f459598eb3cf7f3fa3004af4e71b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SenchaTouchRailsSandbox::Application.config.session_store :cookie_store, key: '_sencha-touch-rails-sandbox_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SenchaTouchRailsSandbox::Application.config.session_store :active_record_store
